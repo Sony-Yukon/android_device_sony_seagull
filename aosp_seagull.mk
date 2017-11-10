975effbfabd11ec2a168022dd301e33f9f3fbd68
Ramón Hernández García <rmnhg16@gmail.com>
2017-11-10 18:31:45 +0100

    seagull: aosp_seagull: update config

--- aosp_seagull.mk
+++ aosp_seagull.mk
@@ -1,4 +1,4 @@
-# Copyright 2014 The Android Open Source Project
+# Copyright 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Kernel config
 TARGET_KERNEL_CONFIG := aosp_yukon_seagull_defconfig
 
 # Inherit from those products. Most specific first.
@@ -19,8 +20,21 @@ $(call inherit-product, device/sony/seagull/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := aosp_d5103
+# Product attributes
+PRODUCT_NAME := aosp_seagull
 PRODUCT_DEVICE := seagull
-PRODUCT_MODEL := Xperia T3 (AOSP)
+PRODUCT_MODEL := Xperia T3
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
+
+$(call inherit-product, device/sony/seagull/device.mk)
+$(call inherit-product-if-exists, vendor/sony/seagull/seagull-vendor.mk)
+
+# Fingerprint for seagull (from stock)
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5103
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D5103/D5103:4.4.2/18.1.A.1.21/6vt_bw:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D5103-user 4.4.2 18.1.A.1.21 6vt_bw release-keys"
+
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
